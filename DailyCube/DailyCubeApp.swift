5867fd6a7b3dfdc3c6fda34b08836d2e3643235e
Chris Henry <cwhenry101@gmail.com>
2025-08-26 10:15:10 -0400

    Initial Commit

---
+++ DailyCube/DailyCubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyCubeApp.swift
+//  DailyCube
+//
+//  Created by Chris Henry on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyCubeTests/DailyCubeTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyCubeTests.swift
+//  DailyCubeTests
+//
+//  Created by Chris Henry on 8/26/25.
+//
+
+import Testing
+@testable import DailyCube
+
+struct DailyCubeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
